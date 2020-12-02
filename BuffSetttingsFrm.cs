07b0703b148b90fa20222b388e0b40b11c1fc31e
nt153133 <francis.magretto@gmail.com>
2020-12-02 17:18:33 -0500

    First Commit

---
+++ BuffSetttingsFrm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LlamaLibrary
+{
+    public partial class BuffSetttingsFrm : Form
+    {
+        public BuffSetttingsFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void BuffSetttingsFrm_Load(object sender, EventArgs e)
+        {
+            propertyGrid1.SelectedObject = FCActionPlugin.Settings;
+        }
+    }
+}
